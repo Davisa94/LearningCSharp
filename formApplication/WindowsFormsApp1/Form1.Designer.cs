8efefdb6e22637d3abeb5746ef2e548f9f66a041
davisa94 <austin.i.davis94@gmail.com>
2019-03-06 13:58:44 -0700

    ignoring more sensetive files

--- formApplication/WindowsFormsApp1/Form1.Designer.cs
+++ formApplication/WindowsFormsApp1/Form1.Designer.cs
@@ -28,18 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.schedulingWarehouseDataSet = new WindowsFormsApp1.SchedulingWarehouseDataSet();
+            this.courseinfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.course_infoTableAdapter = new WindowsFormsApp1.SchedulingWarehouseDataSetTableAdapters.course_infoTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulingWarehouseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseinfoBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 24);
+            this.dataGridView1.DataSource = this.courseinfoBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(754, 403);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 416);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // schedulingWarehouseDataSet
+            // 
+            this.schedulingWarehouseDataSet.DataSetName = "SchedulingWarehouseDataSet";
+            this.schedulingWarehouseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // courseinfoBindingSource
+            // 
+            this.courseinfoBindingSource.DataMember = "course_info";
+            this.courseinfoBindingSource.DataSource = this.schedulingWarehouseDataSet;
+            // 
+            // course_infoTableAdapter
+            // 
+            this.course_infoTableAdapter.ClearBeforeFill = true;
             // 
             // Form1
             // 
@@ -49,7 +72,10 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulingWarehouseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseinfoBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +83,9 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private SchedulingWarehouseDataSet schedulingWarehouseDataSet;
+        private System.Windows.Forms.BindingSource courseinfoBindingSource;
+        private SchedulingWarehouseDataSetTableAdapters.course_infoTableAdapter course_infoTableAdapter;
     }
 }
 
--- formApplication/WindowsFormsApp1/Form1.cs
+++ formApplication/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,17 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'schedulingWarehouseDataSet.course_info' table. You can move, or remove it, as needed.
+            this.course_infoTableAdapter.Fill(this.schedulingWarehouseDataSet.course_info);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
